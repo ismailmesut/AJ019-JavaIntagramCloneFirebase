ccec377919ca411936fe3c73f36427d6c2429b6b
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-28 15:22:36 +0300

    updated: signOut codes

--- app/src/main/java/com/ismailmesutmujde/javaintagramclonefirebase/FeedActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaintagramclonefirebase/FeedActivity.java
@@ -1,16 +1,23 @@
 package com.ismailmesutmujde.javaintagramclonefirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.ismailmesutmujde.javaintagramclonefirebase.databinding.ActivityFeedBinding;
 
 
 public class FeedActivity extends AppCompatActivity {
 
     private ActivityFeedBinding feedBinding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,30 @@ public class FeedActivity extends AppCompatActivity {
         View feedView = feedBinding.getRoot();
         setContentView(feedView);
 
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_post) {
+            // Upload Activity
+            Intent intentToUpload = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentToUpload);
+        } else if (item.getItemId() == R.id.signout) {
+            // Signout
+            auth.signOut();
+            Intent intentToMain = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToMain);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
