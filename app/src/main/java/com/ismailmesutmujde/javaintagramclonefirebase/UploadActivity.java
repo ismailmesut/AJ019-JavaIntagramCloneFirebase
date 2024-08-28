00b6091b10d11bc4f2032cfaa8a895dea5e68761
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-28 11:39:32 +0300

    updated: ui design

--- app/src/main/java/com/ismailmesutmujde/javaintagramclonefirebase/UploadActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaintagramclonefirebase/UploadActivity.java
@@ -5,23 +5,20 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class MainActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload);
+    }
+
+    public void uploadButtonClicked(View view) {
 
     }
 
-    // Giriş Yap
-    public void signInClicked(View view) {
+    public void selectImageClicked(View view) {
 
     }
 
-    // Kayıt Ol
-    public void signUpClicked(View view) {
-
-    }
 }
